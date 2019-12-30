bcc5c34610e541508321322e7d5ed369c867b18a
alexandrujeman <alexandrujeman.ja@gmail.com>
2019-12-30 14:26:32 +0200

    Create API TestView django restframework

--- server/src/core/views.py
+++ server/src/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 # Create your views here.
+
+
+class TestView(APIView):
+    def get(self, request, *args, **kwargs):
+        data = {
+            'name': 'Alex',
+            'age': 23
+        }
+        return Response(data)
--- server/src/rest_api/urls.py
+++ server/src/rest_api/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+from django.urls import path, include
+from core.views import TestView
 
 urlpatterns = [
+    url(r'^api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
+    url(r'^api', TestView.as_view(), name='test'),
 ]
